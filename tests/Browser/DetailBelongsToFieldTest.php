187f0430673ef19ba6c693f5a48d69ff2cdecd49
Taylor Otwell <taylor@laravel.com>
2018-07-11 16:22:10 -0500

    wip

---
+++ tests/Browser/DetailBelongsToFieldTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Post;
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Support\Facades\Hash;
+use Tests\Browser\Components\IndexComponent;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class DetailBelongsToFieldTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function belongs_to_field_navigates_to_parent_resource_when_clicked()
+    {
+        $this->seed();
+
+        $user = User::find(1);
+        $user->posts()->save($post = factory(Post::class)->create());
+
+        $this->browse(function (Browser $browser) use ($user, $post) {
+            $browser->loginAs(User::find(1))
+                    ->visit(new Pages\Detail('posts', $post->id))
+                    ->clickLink($user->name)
+                    ->pause(250)
+                    ->assertPathIs('/nova/resources/users/'.$user->id);
+        });
+    }
+}
