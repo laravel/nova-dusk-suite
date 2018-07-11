2396c04b3f6b0bdfe1cd8add342e2b4978013a99
Taylor Otwell <taylor@laravel.com>
2018-07-11 09:19:01 -0500

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-
-        App\User::forceCreate([
-            'name' => 'Taylor Otwell',
-            'email' => 'taylor@laravel.com',
-            'password' => '$2y$10$oGhaPFdmduG9419sPVkrROY3joLf0iNwxplM5UzcD.x7u06KcNJj6',
-        ]);
+        $this->call(UserTableSeeder::class);
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::forceCreate([
+            'name' => 'Taylor Otwell',
+            'email' => 'taylor@laravel.com',
+            'password' => '$2y$10$oGhaPFdmduG9419sPVkrROY3joLf0iNwxplM5UzcD.x7u06KcNJj6',
+        ]);
+    }
+}
