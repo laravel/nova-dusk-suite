f2a19b5297945ec429ebe98cb599f20ae4398f2f
Taylor Otwell <taylor@laravel.com>
2018-07-12 12:53:21 -0500

    wip

--- app/Dock.php
+++ app/Dock.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dock extends Model
 {
+    use SoftDeletes;
+
     /**
      * Get all of the ships that belongs to the dock.
      */
--- database/migrations/2018_07_12_171623_create_docks_table.php
+++ database/migrations/2018_07_12_171623_create_docks_table.php
@@ -17,6 +17,7 @@ class CreateDocksTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
