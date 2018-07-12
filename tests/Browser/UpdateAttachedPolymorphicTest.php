2950d6f67687a89630328bbe890836ef36747d59
Taylor Otwell <taylor@laravel.com>
2018-07-12 17:09:06 -0500

    wip

--- tests/Browser/UpdateAttachedPolymorphicTest.php
+++ tests/Browser/UpdateAttachedPolymorphicTest.php
@@ -39,6 +39,35 @@ class UpdateAttachedPolymorphicTest extends DuskTestCase
     /**
      * @test
      */
+    public function attached_searchable_resource_is_locked()
+    {
+        $this->seed();
+
+        touch(base_path('.searchable'));
+
+        $post = factory(Post::class)->create();
+        $tag = factory(Tag::class)->create();
+        $post->tags()->attach($tag, ['notes' => 'Test Notes']);
+
+        try {
+            $this->browse(function (Browser $browser) {
+                $browser->loginAs(User::find(1))
+                        ->visit(new Pages\UpdateAttached('posts', 1, 'tags', 1))
+                        ->assertDisabled('@attachable-select')
+                        ->assertInputValue('@notes', 'Test Notes')
+                        ->type('@notes', 'Test Notes Updated')
+                        ->update();
+
+                $this->assertEquals('Test Notes Updated', Post::find(1)->tags->first()->pivot->notes);
+            });
+        } finally {
+            @unlink(base_path('.searchable'));
+        }
+    }
+
+    /**
+     * @test
+     */
     public function attached_resource_can_be_updated_and_can_continue_editing()
     {
         $this->seed();
