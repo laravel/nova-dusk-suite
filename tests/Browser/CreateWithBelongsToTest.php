c1cd5092eaa6b441dcd27944a7956a806220f1cd
Taylor Otwell <taylor@laravel.com>
2018-07-13 10:25:39 -0500

    wip

--- tests/Browser/CreateWithBelongsToTest.php
+++ tests/Browser/CreateWithBelongsToTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Dock;
 use App\Post;
 use App\User;
 use Tests\DuskTestCase;
@@ -60,6 +61,51 @@ class CreateWithBelongsToTest extends DuskTestCase
             $post = $user->posts->first();
             $this->assertEquals('Test Post', $post->title);
             $this->assertEquals('Test Post Body', $post->body);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function searchable_resource_can_be_created()
+    {
+        $this->seed();
+
+        $dock = factory(Dock::class)->create();
+
+        $this->browse(function (Browser $browser) use ($dock) {
+            $browser->loginAs(User::find(1))
+                    ->visit(new Pages\Create('ships'))
+                    ->searchRelation('docks', '1')
+                    ->selectCurrentRelation('docks')
+                    ->type('@name', 'Test Ship')
+                    ->create();
+
+            $this->assertCount(1, $dock->fresh()->ships);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function searchable_parent_resource_should_be_locked_when_creating_via_parents_detail_page()
+    {
+        $this->seed();
+
+        $dock = factory(Dock::class)->create();
+
+        $this->browse(function (Browser $browser) use ($dock) {
+            $browser->loginAs(User::find(1))
+                    ->visit(new Pages\Detail('docks', 1))
+                    ->within(new IndexComponent('ships'), function ($browser) {
+                        $browser->click('@create-button');
+                    })
+                    ->on(new Pages\Create('ships'))
+                    ->assertDisabled('@dock')
+                    ->type('@name', 'Test Ship')
+                    ->create();
+
+            $this->assertCount(1, $dock->fresh()->ships);
         });
     }
 }
