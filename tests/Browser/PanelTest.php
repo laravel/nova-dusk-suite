cec430817ba68c3ff2ca5f98d1a18a88340f6752
Taylor Otwell <taylor@laravel.com>
2018-07-16 18:27:09 +0000

    Apply fixes from StyleCI

--- tests/Browser/PanelTest.php
+++ tests/Browser/PanelTest.php
@@ -6,7 +6,6 @@ use App\User;
 use App\Address;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class PanelTest extends DuskTestCase
