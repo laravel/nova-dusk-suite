d4c28c9992315de06415e37f012bc82bda923e61
Taylor Otwell <taylor@laravel.com>
2018-08-13 14:17:42 -0500

    wip

--- nova-components/SidebarTool/routes/api.php
+++ nova-components/SidebarTool/routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Otwell\SidebarTool\Http\Middleware\Authorize;
-=======
->>>>>>> 7fea1501482f448cc8cc5a4eeac2c908f072d07d
 
 Route::middleware(['nova', Authorize::class])->group(function () {
     Route::get('/nova-api/custom-sidebar-tool', function (Request $request) {
