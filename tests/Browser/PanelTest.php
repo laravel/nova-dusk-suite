b3f30d8bdbaf2926584f79bbc0b751745d1a8648
Courtney Trudel <courtney@plankdesign.com>
2019-05-03 16:37:07 -0400

    Added panel test for update view

--- tests/Browser/PanelTest.php
+++ tests/Browser/PanelTest.php
@@ -27,4 +27,20 @@ class PanelTest extends DuskTestCase
                     ->assertSee('More Address Details');
         });
     }
+
+    /**
+     * @test
+     */
+    public function fields_can_be_placed_into_edit_panels()
+    {
+        $this->seed();
+
+        $address = factory(Address::class)->create();
+
+        $this->browse(function (Browser $browser) use ($address) {
+            $browser->loginAs(User::find(1))
+                ->visit(new Pages\Update('addresses', $address->id))
+                ->assertSee('More Address Details');
+        });
+    }
 }
