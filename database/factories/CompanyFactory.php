25da9188cc49e929c40eb70a439c7034d6929625
Mior Muhammad Zaki <crynobone@gmail.com>
2020-10-16 10:07:50 +0800

    Add company example.

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Company;
+use Faker\Generator as Faker;
+
+$factory->define(Company::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
