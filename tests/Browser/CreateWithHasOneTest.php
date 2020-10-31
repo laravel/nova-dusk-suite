37435018e0f099f983d8abf0fff320fd05d2d026
anditsung <anditsung@gmail.com>
2020-10-31 11:03:11 +0700

    update test class

--- tests/Browser/CreateWithHasOneTest.php
+++ tests/Browser/CreateWithHasOneTest.php
@@ -1,10 +1,13 @@
 <?php
 
+
 namespace Laravel\Nova\Tests\Browser;
+
 
 use App\Models\User;
 use Laravel\Dusk\Browser;
 use Laravel\Nova\Testing\Browser\Components\IndexComponent;
+use Laravel\Nova\Testing\Browser\Pages\Create;
 use Laravel\Nova\Testing\Browser\Pages\Detail;
 use Laravel\Nova\Tests\DuskTestCase;
 
@@ -16,12 +19,18 @@ class CreateWithHasOneTest extends DuskTestCase
 
         $this->browse(function (Browser $browser) {
             $browser->loginAs(User::find(1))
-                ->visit(new Detail('users', 1))
-                ->within(new IndexComponent('addresses'), function ($browser) {
+                ->visit(new Create('people'))
+                ->type('@name', 'Adam Wathan')
+                ->create()
+                ->visit(new Detail('people', 1))
+                ->within(new IndexComponent('employees'), function ($browser) {
                     $browser->click('@create-button');
                 })
                 ->pause(200)
-                ->assertDisabled('@user');
+                ->assertDisabled('@people');
+
+            $browser->blank();
         });
     }
+
 }
