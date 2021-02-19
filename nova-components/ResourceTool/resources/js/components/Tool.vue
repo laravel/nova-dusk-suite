9cc9cf67497eb7141c7f6dfe9ba8e8341b87b57e
Mior Muhammad Zaki <crynobone@gmail.com>
2021-02-19 22:28:21 +0800

    Update tool.js
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/ResourceTool/resources/js/components/Tool.vue
+++ nova-components/ResourceTool/resources/js/components/Tool.vue
@@ -6,7 +6,7 @@
 
 <script>
     export default {
-        props: ['resourceName', 'resourceId', 'field'],
+        props: ['resourceName', 'resourceId', 'resource', 'panel'],
 
         mounted() {
             //
