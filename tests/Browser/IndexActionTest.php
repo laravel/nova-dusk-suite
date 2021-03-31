54b4f6ef6cfab14ad4cadff34f31ef70cb60e0d8
Mior Muhammad Zaki <crynobone@gmail.com>
2021-03-31 12:09:33 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/IndexActionTest.php
+++ tests/Browser/IndexActionTest.php
@@ -2,10 +2,13 @@
 
 namespace Laravel\Nova\Tests\Browser;
 
+use App\Models\Post;
 use App\Models\User;
+use Database\Factories\PostFactory;
 use Database\Factories\UserFactory;
 use Laravel\Dusk\Browser;
 use Laravel\Nova\Testing\Browser\Components\IndexComponent;
+use Laravel\Nova\Testing\Browser\Pages\Index;
 use Laravel\Nova\Testing\Browser\Pages\UserIndex;
 use Laravel\Nova\Tests\DuskTestCase;
 
@@ -20,7 +23,7 @@ class IndexActionTest extends DuskTestCase
             $browser->loginAs(User::find(1))
                     ->visit(new UserIndex)
                     ->within(new IndexComponent('users'), function ($browser) {
-                        $browser->waitForTable(25)
+                        $browser->waitForTable()
                             ->clickCheckboxForId(3)
                             ->clickCheckboxForId(2)
                             ->runAction('mark-as-active');
@@ -29,6 +32,51 @@ class IndexActionTest extends DuskTestCase
             $this->assertEquals(0, User::find(1)->active);
             $this->assertEquals(1, User::find(2)->active);
             $this->assertEquals(1, User::find(3)->active);
+
+            $browser->blank();
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_run_actions_on_deleted_resources()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit(new UserIndex)
+                    ->within(new IndexComponent('users'), function ($browser) {
+                        $browser->waitForTable()
+                            ->clickCheckboxForId(3);
+
+                        User::where('id', '=', 3)->delete();
+
+                        $browser->runAction('mark-as-active');
+                    })->waitForText('Sorry! You are not authorized to perform this action.')
+                    ->assertSee('Sorry! You are not authorized to perform this action.');
+
+            $browser->blank();
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_run_standalone_actions_on_deleted_resource()
+    {
+        PostFactory::new()->times(5)->create();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit(new Index('posts'))
+                    ->within(new IndexComponent('posts'), function ($browser) {
+                        $browser->waitForTable();
+
+                        Post::query()->delete();
+
+                        $browser->runAction('standalone-task');
+                    })->waitForText('Action executed!')
+                    ->assertSee('Action executed!');
 
             $browser->blank();
         });
@@ -47,7 +95,7 @@ class IndexActionTest extends DuskTestCase
             $browser->loginAs(User::find(1))
                     ->visit(new UserIndex)
                     ->within(new IndexComponent('users'), function ($browser) {
-                        $browser->waitForTable(25)
+                        $browser->waitForTable()
                             ->selectAllMatching()
                             ->runAction('mark-as-active');
                     })->waitForText('The action ran successfully!');
@@ -69,7 +117,7 @@ class IndexActionTest extends DuskTestCase
             $browser->loginAs(User::find(1))
                     ->visit(new UserIndex)
                     ->within(new IndexComponent('users'), function ($browser) {
-                        $browser->waitForTable(25)
+                        $browser->waitForTable()
                             ->assertDontSeeIn('@1-row', 'Mark As Inactive')
                             ->assertSeeIn('@2-row', 'Mark As Inactive')
                             ->runInlineAction(2, 'mark-as-inactive');
