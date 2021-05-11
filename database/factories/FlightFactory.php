eb5cba6e5995157869d5ed5359bbd6d432286ec4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-05-11 20:16:57 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -22,7 +22,7 @@ class FlightFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->word(),
         ];
     }
 }
