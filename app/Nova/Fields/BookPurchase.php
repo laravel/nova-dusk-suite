7c69e7225f0bad2eee4d2c8c21554278bf5d44b4
Mior Muhammad Zaki <crynobone@gmail.com>
2021-06-17 17:25:11 +0800

    Add tests example for Book Purchase.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Fields/BookPurchase.php
+++ app/Nova/Fields/BookPurchase.php
@@ -2,25 +2,28 @@
 
 namespace App\Nova\Fields;
 
+use App\Nova\Book;
 use Laravel\Nova\Fields\Currency;
 use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Resource;
 
 class BookPurchase
 {
     /**
      * Purchase type.
      *
-     * @var string
+     * @var string|null
      */
     protected $type;
 
     /**
      * Construct a new object.
      *
-     * @param string $type
+     * @param string|null  $type
      */
-    public function __construct($type = 'personal')
+    public function __construct($type = null)
     {
         $this->type = $type;
     }
@@ -33,12 +36,28 @@ class BookPurchase
     public function __invoke()
     {
         return [
-            Currency::make('Price'),
-            Select::make('Type')->options([
-                'personal' => 'Personal',
-                'gift' => 'Gift',
-            ])->default($this->type),
-            DateTime::make('Purchased At'),
+            Currency::make('Price')
+                ->dependsOn(['books'], function ($field, NovaRequest $request, $fields) {
+                    $bookId = (int) (
+                        $request->resource === 'books' ? $request->resourceId : $fields->books
+                    );
+
+                    if ($bookId == 1) {
+                        $field->rules(['required', 'numeric', 'min:10', 'max:199']);
+                    }
+                })->rules(['required', 'numeric', 'min:0', 'max:199']),
+
+            Select::make('Type')
+                ->options([
+                    'personal' => 'Personal',
+                    'gift' => 'Gift',
+                ])
+                ->default($this->type ?? 'personal')
+                ->readonly(function () {
+                    return ! is_null($this->type);
+                }),
+
+            DateTime::make('Purchased At')->rules('required'),
         ];
     }
 }
