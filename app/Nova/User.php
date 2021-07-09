97d56b2c998ae89d4c30badb53db3e29c4a9acdc
Mior Muhammad Zaki <crynobone@gmail.com>
2021-07-09 16:17:18 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -175,4 +175,16 @@ class User extends Resource
             new Filters\Created,
         ];
     }
+
+    /**
+     * Return the location to redirect the user after creation.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Laravel\Nova\Resource  $resource
+     * @return string
+     */
+    public static function redirectAfterCreate(NovaRequest $request, $resource)
+    {
+        return '/resources/profiles/new?viaResource='.static::uriKey().'&viaResourceId='.$resource->getKey().'&viaRelationship=profile';
+    }
 }
