bd416849de14349b62f2a5a00226a6c945fbc3d7
Mior Muhammad Zaki <crynobone@gmail.com>
2021-07-26 21:56:54 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/SidebarTool/tailwind.config.js
+++ nova-components/SidebarTool/tailwind.config.js
@@ -1,11 +1,7 @@
 let novaConfig = require('./vendor/laravel/nova/tailwind.config.js')
 
 module.exports = {
+  ...novaConfig,
   mode: 'jit',
   purge: ['./src/**/*.php', './resources/**/*.{js,vue,blade.php}'],
-  theme: {
-    extend: novaConfig.theme.extend
-  },
-  variants: {},
-  plugins: []
 };
