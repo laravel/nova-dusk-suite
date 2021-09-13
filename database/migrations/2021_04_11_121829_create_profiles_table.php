e38b237f824752b170365877c94f8c09ea039ad1
Mior Muhammad Zaki <crynobone@gmail.com>
2021-09-13 08:33:12 +0800

    Use unique.

--- database/migrations/2021_04_11_121829_create_profiles_table.php
+++ database/migrations/2021_04_11_121829_create_profiles_table.php
@@ -15,7 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->index();
+            $table->foreignId('user_id')->unique();
 
             $table->string('github_url')->nullable();
             $table->string('twitter_url')->nullable();
