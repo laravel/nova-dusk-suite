cff046a66620ffb3875edd182c3809a506effe35
Mior Muhammad Zaki <crynobone@gmail.com>
2021-09-13 10:20:51 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/ApplicationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Laravel\Nova\Tests\Feature;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Nova\Tests\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Ignore package discovery from.
+     *
+     * @return array
+     */
+    public function ignorePackageDiscoveriesFrom()
+    {
+        return ['*'];
+    }
+
+    /** @test */
+    public function it_discovers_required_packages()
+    {
+        $this->assertTrue($this->app->providerIsLoaded(\Inertia\ServiceProvider::class));
+        $this->assertTrue($this->app->providerIsLoaded(\Laravel\Nova\NovaServiceProvider::class));
+
+    }
+}
