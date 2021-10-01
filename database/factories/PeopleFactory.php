2ab45bf1bc951e4d5863a789398cc965343989ef
Mior Muhammad Zaki <crynobone@gmail.com>
2021-10-01 14:54:32 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ database/factories/PeopleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\People;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PeopleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = People::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
