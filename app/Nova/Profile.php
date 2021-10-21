edafc10028cd8d5e80493eb97ba127a522e1153c
Mior Muhammad Zaki <crynobone@gmail.com>
2021-10-21 08:50:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Profile.php
+++ app/Nova/Profile.php
@@ -3,6 +3,7 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\HasOne;
 use Laravel\Nova\Fields\ID;
