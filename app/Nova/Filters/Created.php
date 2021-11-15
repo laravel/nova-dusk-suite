0d149b7b845adeed86acb98f04763def7e4e1670
Mior Muhammad Zaki <crynobone@gmail.com>
2021-11-15 09:27:40 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Filters/Created.php
+++ app/Nova/Filters/Created.php
@@ -13,7 +13,7 @@ class Created extends DateFilter
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  mixed  $value
+     * @param  \DateTimeInterface|string|null  $value
      * @return \Illuminate\Database\Eloquent\Builder
      */
     public function apply(Request $request, $query, $value)
