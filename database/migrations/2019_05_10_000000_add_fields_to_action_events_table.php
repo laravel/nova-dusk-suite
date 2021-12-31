14c92f7f56f1a50d1c69f8833a00d6a383c4de8c
Mior Muhammad Zaki <crynobone@gmail.com>
2021-12-31 18:34:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2019_05_10_000000_add_fields_to_action_events_table.php
+++ database/migrations/2019_05_10_000000_add_fields_to_action_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToActionEventsTable extends Migration
+new class() extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class AddFieldsToActionEventsTable extends Migration
             $table->dropColumn('original', 'changes');
         });
     }
-}
+};
