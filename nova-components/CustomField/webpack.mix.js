e1412bc90b32926564fabb16fa1088a1b268e566
Mior Muhammad Zaki <crynobone@gmail.com>
2022-02-22 10:45:30 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/CustomField/webpack.mix.js
+++ nova-components/CustomField/webpack.mix.js
@@ -8,7 +8,4 @@ mix
   .js('resources/js/field.js', 'js')
   .vue({ version: 3 })
   .css('resources/css/field.css', 'css')
-  .alias({
-    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js'),
-  })
   .nova('otwell/custom-field')
