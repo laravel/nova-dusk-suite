b074fd87ced5275b00a074b619b17a60036a07d6
Mior Muhammad Zaki <crynobone@gmail.com>
2022-02-22 10:48:41 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/ResourceTool/webpack.mix.js
+++ nova-components/ResourceTool/webpack.mix.js
@@ -7,22 +7,5 @@ mix
   .setPublicPath('dist')
   .js('resources/js/tool.js', 'js')
   .vue({ version: 3})
-  .alias({
-    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js'),
-  })
   .nova('otwell/resource-tool')
-  .webpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.(postcss)$/,
-          use: [
-            'vue-style-loader',
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader'
-          ]
-        }
-      ],
-    },
-  })
 
