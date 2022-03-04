afebecff0e55edcac7ec105fb35c5d1923a5db62
Mior Muhammad Zaki <crynobone@gmail.com>
2022-03-04 08:38:49 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/ResourceTool/resources/js/tool.js
+++ nova-components/ResourceTool/resources/js/tool.js
@@ -1,3 +1,4 @@
 Nova.booting(function (app, store) {
+  app.component('resource-tool', require('./components/Tool').default);
   app.component('detail-resource-tool', require('./components/Tool').default);
 })
