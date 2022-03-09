17eadea7fc8e79a45f3467153b35902cdf352062
Mior Muhammad Zaki <crynobone@gmail.com>
2022-03-09 17:12:00 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -148,7 +148,7 @@ class User extends Authenticatable
      */
     public function canImpersonate()
     {
-        return in_array($this->email, ['taylor@laravel.com', 'themsaid@laravel.com', 'david@laravel.com', 'nova@laravel.com']);
+        return in_array($this->email, ['taylor@laravel.com', 'mohamed@laravel.com', 'david@laravel.com', 'nova@laravel.com']);
     }
 
     /**
