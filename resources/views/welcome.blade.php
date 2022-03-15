e13125e873f7a1b935750045f10fef73ae13a704
Mior Muhammad Zaki <crynobone@gmail.com>
2022-03-16 06:59:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,9 @@
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        @can('viewNova')
+                        <a href="{{ url('/nova') }}" class="text-sm text-gray-700 underline">Nova</a>
+                        @endcan
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
