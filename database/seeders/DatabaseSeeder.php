d9896bf5031f2002307bb61029cdaac9701f2996
Mior Muhammad Zaki <crynobone@gmail.com>
2022-03-22 09:09:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(ProfileTableSeeder::class);
-        $this->call(SubscriberTableSeeder::class);
         $this->call(BookTableSeeder::class);
     }
 }
