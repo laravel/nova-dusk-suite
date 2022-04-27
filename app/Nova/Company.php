d04c6db24626dceaca9c88f59fa323ab5739fed4
Mior Muhammad Zaki <crynobone@gmail.com>
2022-04-27 22:49:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Company.php
+++ app/Nova/Company.php
@@ -2,10 +2,10 @@
 
 namespace App\Nova;
 
+use App\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\Country;
 use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
---
+++ app/Nova/Fields/Markdown.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Nova\Fields;
+
+use Laravel\Nova\Fields\SupportsDependentFields;
+
+class Markdown extends \Laravel\Nova\Fields\Markdown
+{
+    use SupportsDependentFields;
+}
