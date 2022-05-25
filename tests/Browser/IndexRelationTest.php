20992071638d057489fd72ad29bf5b44b0f290b6
Mior Muhammad Zaki <crynobone@gmail.com>
2022-05-25 11:25:29 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/IndexRelationTest.php
+++ tests/Browser/IndexRelationTest.php
@@ -131,8 +131,8 @@ class IndexRelationTest extends DuskTestCase
                                 ->deleteSelected();
                     });
 
-            $this->assertNull($post->fresh());
-            $this->assertNotNull($post2->fresh());
+            $this->assertNull(Post::find($post->id));
+            $this->assertNotNull(Post::find($post2->id));
 
             $browser->blank();
         });
