c8206a324b973cc86fa2c78576be37fcba012397
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-02 19:22:18 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Role.php
+++ app/Nova/Role.php
@@ -56,7 +56,7 @@ class Role extends Resource
                         })
                         ->prunable()
                         ->filterable()
-                        ->reorderAttachables(! file_exists('disable-reordering')),
+                        ->reorderAttachables(! file_exists(base_path('.disable-reordering'))),
         ];
     }
 
