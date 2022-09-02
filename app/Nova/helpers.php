41f1e5adea099999668c21b17bcaa3a1b967841c
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-02 23:15:42 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/helpers.php
+++ app/Nova/helpers.php
@@ -4,21 +4,15 @@ namespace App\Nova;
 
 function uses_searchable()
 {
-    return function () {
-        return app('uses_searchable');
-    };
+    return app('uses_searchable');
 }
 
 function uses_inline_create()
 {
-    return function () {
-        return app('uses_inline_create');
-    };
+    return app('uses_inline_create');
 }
 
 function uses_without_reordering()
 {
-    return function () {
-        return app('uses_without_reordering');
-    };
+    return app('uses_without_reordering');
 }
