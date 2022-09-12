c9faf7ca70a9f14c139bfaa0d988ab2b1654d8bc
Mior Muhammad Zaki <crynobone@gmail.com>
2022-09-12 20:51:50 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,7 +12,12 @@ function uses_inline_create()
     return app('uses_inline_create');
 }
 
+function uses_with_reordering()
+{
+    return app('uses_with_reordering');
+}
+
 function uses_without_reordering()
 {
-    return app('uses_without_reordering');
+    return ! app('uses_with_reordering');
 }
