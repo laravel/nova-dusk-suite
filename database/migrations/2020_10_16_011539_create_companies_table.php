10adc1187c175271b21bb4a992d95ee26b9e5439
Mior Muhammad Zaki <crynobone@gmail.com>
2022-11-30 11:06:08 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2020_10_16_011539_create_companies_table.php
+++ database/migrations/2020_10_16_011539_create_companies_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('country')->nullable();
-            $table->string('description')->nullable();
+            $table->longText('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
