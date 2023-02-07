09269cba45aab6d392a8cbf20631d2fb18826d26
Mior Muhammad Zaki <crynobone@gmail.com>
2023-02-07 08:37:56 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('active')->default(false);
+            $table->string('blocked_from', 1000)->default('{}');
             $table->rememberToken();
             $table->timestamps();
         });
