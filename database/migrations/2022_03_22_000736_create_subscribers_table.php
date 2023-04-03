8b7f194e2ab229bd82dbfc8302409c13390b773b
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-03 12:12:24 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/migrations/2022_03_22_000736_create_subscribers_table.php
+++ database/migrations/2022_03_22_000736_create_subscribers_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->json('meta')->nullable();
             $table->timestamps();
         });
     }
