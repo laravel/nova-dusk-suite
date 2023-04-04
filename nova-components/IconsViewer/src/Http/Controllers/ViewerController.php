0e7c7229312b7cc322e08417f35776e1f849e8d2
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-04 08:00:50 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- nova-components/IconsViewer/src/Http/Controllers/ViewerController.php
+++ nova-components/IconsViewer/src/Http/Controllers/ViewerController.php
@@ -5,18 +5,25 @@ namespace Otwell\IconsViewer\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\LazyCollection;
 use Illuminate\Support\Str;
+use Inertia\Inertia;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Symfony\Component\Finder\Finder;
 
 class ViewerController extends Controller
 {
+    /**
+     * Show the icons.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return \Inertia\Response
+     */
     public function __invoke(NovaRequest $request)
     {
-        return inertia('IconsViewer', [
-            'icons' => ray()->pass([
+        return Inertia::render('IconsViewer', [
+            'icons' => [
                 'solid' => $this->iconSet('solid'),
                 'outline' => $this->iconSet('outline'),
-            ]),
+            ],
         ]);
     }
 
@@ -26,9 +33,8 @@ class ViewerController extends Controller
      * @param  string  $set
      * @return array
      */
-    public static function iconSet($set)
+    public static function iconSet(string $set)
     {
-        /** @phpstan-ignore-next-line */
         $directory = NOVA_PATH.'/resources/js/components/Heroicons/'.$set;
 
         return LazyCollection::make(function () use ($directory) {
