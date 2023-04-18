598bb328007ce888f42b2cd0dedf5cbf4435c26d
Mior Muhammad Zaki <crynobone@gmail.com>
2023-04-18 20:29:11 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,13 @@ use Orchestra\Testbench\Dusk\Options;
 
 require __DIR__.'/../vendor/autoload.php';
 
-if (isset($_ENV['APP_SERVED']) || isset($_SERVER['APP_SERVED'])) {
-    Options::$providesApplicationServer = false;
+if (isset($_SERVER['CI']) || isset($_ENV['CI'])) {
+    Options::withoutUI();
+} else {
+    Options::withUI();
 }
+
+Options::$w3cCompliant = false;
 
 Options::addArgument('--disable-popup-blocking');
 Options::addArgument('--force-prefers-reduced-motion');
