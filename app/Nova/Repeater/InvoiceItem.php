3e5229829f3a6de15d02a69706014f7b757114fb
Mior Muhammad Zaki <crynobone@gmail.com>
2023-06-27 06:12:56 +0800

    Test Improvements (#174)
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * Apply fixes from StyleCI
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ app/Nova/Repeater/InvoiceItem.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Nova\Repeater;
+
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Repeater\Repeatable;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class InvoiceItem extends Repeatable
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\InvoiceItem>
+     */
+    public static $model = \App\Models\InvoiceItem::class;
+
+    /**
+     * Get the fields displayed by the repeatable.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make(),
+
+            Number::make('Quantity')->rules('numeric'),
+            Text::make('Description')->rules('string'),
+            Currency::make('Price')->rules('numeric')->asMinorUnits(),
+        ];
+    }
+}
