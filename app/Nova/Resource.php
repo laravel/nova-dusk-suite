9dbdacf3b724739b2613fe26383023918926a72c
Mior Muhammad Zaki <crynobone@gmail.com>
2023-07-24 08:06:06 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Resource.php
+++ app/Nova/Resource.php
@@ -64,19 +64,19 @@ abstract class Resource extends NovaResource
         return parent::detailQuery($request, $query);
     }
 
-    /**
-     * Build a "relatable" query for the given resource.
-     *
-     * This query determines which instances of the model may be attached to other resources.
-     *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public static function relatableQuery(NovaRequest $request, $query)
-    {
-        return parent::relatableQuery($request, $query);
-    }
+    // /**
+    //  * Build a "relatable" query for the given resource.
+    //  *
+    //  * This query determines which instances of the model may be attached to other resources.
+    //  *
+    //  * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+    //  * @param  \Illuminate\Database\Eloquent\Builder  $query
+    //  * @return \Illuminate\Database\Eloquent\Builder
+    //  */
+    // public static function relatableQuery(NovaRequest $request, $query)
+    // {
+    //     return parent::relatableQuery($request, $query);
+    // }
 
     // /**
     //  * Return a new Action field instance.
