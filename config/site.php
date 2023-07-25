2d2c332d49fa3bcfd56f28c2f93595685075a18a
Mior Muhammad Zaki <crynobone@gmail.com>
2023-07-25 11:29:39 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ config/site.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'resources' => [
+        /*
+         * Set the default ordering for Nova Resources.
+         *
+         * Supported: "id", "created", "updated_at" or null (fallback to "id")
+         */
+        'orderings' => null,
+    ],
+];
