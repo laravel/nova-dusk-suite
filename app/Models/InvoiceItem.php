4e17cd9a6eb567b2a17898dc7cc3efc5a1915f69
Mior Muhammad Zaki <crynobone@gmail.com>
2023-07-26 14:43:18 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InvoiceItem extends Model
 {
+    use SoftDeletes;
+
     /**
      * Get the invoice this item belongs to.
      *
