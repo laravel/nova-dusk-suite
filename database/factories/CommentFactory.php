06d973c872174139d8efeb5033cb9663f346d82d
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-11 12:54:33 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,7 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\Comment;
+use App\Models\Link;
 use App\Models\Post;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -30,5 +32,35 @@ class CommentFactory extends Factory
             'commentable_id' => PostFactory::new(),
             'body' => $this->faker->words(3, true),
         ];
+    }
+
+    /**
+     * Indicate that the model's commentable for videos.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function videos()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'commentable_type' => Video::class,
+                'commentable_id' => VideoFactory::new(),
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the model's commentable for videos.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function links()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'commentable_type' => Link::class,
+                'commentable_id' => LinkFactory::new(),
+            ];
+        });
     }
 }
