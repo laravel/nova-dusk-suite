706350e67090e016608635d398282e62ef85b261
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-28 17:09:29 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Concerns/DatabaseTruncation.php
+++ tests/Concerns/DatabaseTruncation.php
@@ -4,26 +4,17 @@ namespace Laravel\Nova\Tests\Concerns;
 
 use Illuminate\Foundation\Application;
 
-if (version_compare(Application::VERSION, '9.50', '<')) {
-    trait DatabaseTruncation
+trait DatabaseTruncation
+{
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    protected function defineDatabaseMigrations()
     {
-        /**
-         * Define database migrations.
-         *
-         * @return void
-         */
-        protected function defineDatabaseMigrations()
-        {
-            $this->artisan('migrate:fresh', [
-                '--seed' => true,
-            ]);
-        }
-    }
-} else {
-    trait DatabaseTruncation
-    {
-        use \Illuminate\Foundation\Testing\DatabaseTruncation;
-
-        protected $seed = true;
+        $this->artisan('migrate:fresh', [
+            '--seed' => true,
+        ]);
     }
 }
