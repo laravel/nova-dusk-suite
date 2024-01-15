2ed69bf83b8d391912a15834831bf6fb92c8a7d8
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-15 20:48:55 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ define('LARAVEL_START', microtime(true));
  * we will load this file so that any pre-rendered content can be shown
  * instead of starting the framework, which could cause an exception.
  */
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
@@ -26,6 +27,7 @@ require __DIR__.'/../vendor/autoload.php';
  * the application's HTTP kernel. Then, we will send the response back
  * to this client's browser, allowing them to enjoy our application.
  */
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
