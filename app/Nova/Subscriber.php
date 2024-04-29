5745b2ea87d4cb6084c27c696130b58f1eb1ddbb
Mior Muhammad Zaki <crynobone@gmail.com>
2024-04-29 17:29:13 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/Subscriber.php
+++ app/Nova/Subscriber.php
@@ -12,6 +12,7 @@ use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Query\Search;
+use Laravel\Nova\Tabs\Tabs;
 
 /**
  * @template TModel of \App\Models\Subscriber
