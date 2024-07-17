83d18ba97defa109bb2ef0ef82ab733c7810af46
Mior Muhammad Zaki <crynobone@gmail.com>
2024-07-18 07:44:06 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     */
 
     'deprecations' => [
-        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'deprecations'),
         'trace' => false,
     ],
 
