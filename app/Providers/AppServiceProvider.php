6853144cf68bf5f20317bdff0813649bf27b14a0
Jeremy Nikolic <nikolic.jeremy@gmail.com>
2024-09-19 11:55:49 +0200

    Introduce new `uses_without_trashed()` helper (#251)
    * Introduce a new helper uses_without_trashed
    
    * Makes Captain displayWithTrashed conditional for Ships relationships

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->instance('uses_inline_create', file_exists(base_path('.inline-create')));
         $this->app->instance('uses_with_reordering', ! file_exists(base_path('.disable-reordering')));
         $this->app->instance('uses_breadcrumbs', ! file_exists(base_path('.disable-breadcrumbs')));
+        $this->app->instance('uses_without_trashed', file_exists(base_path('.without-trashed')));
     }
 
     /**
