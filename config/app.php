fb69ee7a2754f13196ab30aeeef386499116ef9b
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-25 19:19:40 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\SuiteServiceProvider::class,
     ])->toArray(),
 
     /*
